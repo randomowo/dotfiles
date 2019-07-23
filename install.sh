5b82b1d83eb2911eca6f594ab1f41fe8acee9907
randomowo <randomowo@ya.ru>
2019-07-23 15:07:10 +0300

    removed install script

--- install.sh
+++
@@ -1,8 +0,0 @@
-# zsh
-ln -sv $(pwd)/zsh/.zshrc ~/.zshrc
-ln -sv $(pwd)/zsh/oh-my-zsh ~/.oh-my-zsh
-# vim
-ln -sv $(pwd)/vim/.vimrc ~/.vimrc
-ln -sv $(pwd)/vim/vimconf ~/.vim
-# ranger
-ln -sv $(pwd)/ranger ~/.config/ranger
