351ac4e63a101650f3aa70ac2c4ac825ca5edd74
randomowo <randomowo@ya.ru>
2019-08-15 17:05:09 +0300

    added multiple screens support

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,5 +8,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar default -c ~/.config/polybar/config.ini &
+# Launch
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload default -c $HOME/.config/polybar/config.ini &
+  done
+else
+  polybar --reload default -c $HOME/.config/polybar/config.ini &
+fi
