70cb355ff38ee3ccec857cc1ac5711673ba54523
randomowo <randomowo@ya.ru>
2019-11-08 23:25:59 +0300

    changed font size. added new mode in i3. changed notify sound

--- programms/st/config.h
+++ programms/st/config.h
@@ -7,7 +7,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:size=10:antialias=true:autohint=true";
+static char *font = "Anonymous Pro:size=14";
 static int borderpx = 3;
 
 /*
