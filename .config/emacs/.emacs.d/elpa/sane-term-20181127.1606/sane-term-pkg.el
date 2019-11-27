2373fee38f052bd9425dab8ef7660c72ab66214b
randomowo <randomowo@ya.ru>
2019-11-28 01:46:04 +0300

    removed odd emacs files

--- .config/emacs/.emacs.d/elpa/sane-term-20181127.1606/sane-term-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "sane-term" "20181127.1606" "Multi Term is crazy. This is not." '((emacs "24.1")) :commit "68f4cb4ce090574e96d343021b71969798145128" :authors '(("Adam Patterson" . "adam@adamrt.com")) :maintainer '("Adam Patterson" . "adam@adamrt.com") :url "http://github.com/adamrt/sane-term")
