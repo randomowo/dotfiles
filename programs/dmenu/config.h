2a22413592a4f80191197832c81a0e60c1b1be75
randomowo <randomowo@ya.ru>
2020-01-08 07:32:42 +0300

    changed font to Anonymous Pro

--- programs/dmenu/config.h
+++ programs/dmenu/config.h
@@ -7,7 +7,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"xos4 Terminus:size=12"
+	"Anonymous Pro:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
