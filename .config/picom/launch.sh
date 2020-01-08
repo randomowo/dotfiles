26aeaf01251eb1c0f118a538b27c428a6a9e610a
randomowo <randomowo@ya.ru>
2020-01-08 07:29:04 +0300

    compton -> picom

---
+++ .config/picom/launch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q picom
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x  picom>/dev/null; do sleep 1; done
+
+# Launch
+picom --config ~/.config/picom/config &
