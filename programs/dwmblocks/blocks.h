d5856e91d3d8342f288cd6e71f6ff2adb459835d
randomowo <randomowo@ya.ru>
2020-06-05 22:37:37 +0300

    [dwmblocks], [tmux] * added patch for delimeter * refactored delimeter for dwmblocks * added mouse disabling in tmux

--- programs/dwmblocks/blocks.h
+++ programs/dwmblocks/blocks.h
@@ -1,19 +1,26 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+    /*Icon*/    /*Command*/     /*Update Interval*/ /*Update Signal*/
 
-	{"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_record.sh",		5,		5},
-	{"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_battery.sh",		10,		10},
-	{"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_cputemp.sh",		5,		10},
-	{"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_cpuusage.sh",		5,		10},
-	{"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_backlight.sh",	1,		0},
-	{"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_pulse.sh",		1,		0},
-	{"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_network.sh",		10,		10},
-	{"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_weather.sh",		3600,	60},
-	{"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_date.sh",			1,		0},
-	
+    {"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_record.sh",       5,      5},
+    {"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_battery.sh",      10,     10},
+    {"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_cputemp.sh",      5,      10},
+    {"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_cpuusage.sh",     5,      10},
+    {"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_backlight.sh",    1,      0},
+    {"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_pulse.sh",        1,      0},
+    {"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_network.sh",      10,     10},
+    {"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_weather.sh",      3600,   60},
+    {"", "~/dotfiles/programs/dwmblocks/bar-functions/dwm_date.sh",         1,      0},
 };
 
-// set block limeters (only 2 chars)
-// NULL ('\0') means no limiters
-static char *delim = "[]";
+/*
+   set block limeters
+   set as 2 block delimeter
+   delim = []
+   ]block[]block[
+   delib = ][
+   [block][block]
+   delim = |
+   block|block
+   */
+static char *delim = "}{";
--- programs/dwmblocks/dwmblocks.c
+++ programs/dwmblocks/dwmblocks.c
@@ -5,13 +5,13 @@
 #include<signal.h>
 #include<X11/Xlib.h>
 #define LENGTH(X)               (sizeof(X) / sizeof (X[0]))
-#define CMDLENGTH		50
+#define CMDLENGTH       50
 
 typedef struct {
-	char* icon;
-	char* command;
-	unsigned int interval;
-	unsigned int signal;
+    char* icon;
+    char* command;
+    unsigned int interval;
+    unsigned int signal;
 } Block;
 void sighandler(int num);
 void getcmds(int time);
@@ -37,137 +37,139 @@ static int statusContinue = 1;
 static void (*writestatus) () = setroot;
 
 //opens process *cmd and stores output in *output
-void getcmd(const Block *block, char *output)
+void getcmd(const Block *block, char *output, int isLast)
 {
-	strcpy(output, block->icon);
-	char *cmd = block->command;
-	FILE *cmdf = popen(cmd,"r");
-	if (!cmdf)
-		return;
-	int i = strlen(block->icon);
-	fgets(output+i, CMDLENGTH-i, cmdf);
-	i = strlen(output);
-	if (*delim != '\0' && i > 1) {
-		for (int oi=i; oi >= 0; oi--) {
-			output[oi] = output[oi-1];
-		}
-		output[0] = *delim;
-		output[i++] = *(delim+1);
-	}
-	if (i <= 1)
-		--i;
-	output[i++] = '\0';
-	printf("%s\n", output);
-	pclose(cmdf);
+    char *cmd = block->command;
+    FILE *cmdf = popen(cmd,"r");
+    if (!cmdf) return;
+    int ii = strlen(block->icon);
+    strcpy(output, block->icon);
+    fgets(output+ii, CMDLENGTH-ii, cmdf);
+    int i = strlen(output);
+    int di = strlen(delim);
+    if (i-1 && di) {
+        strncpy(output+(di/2), output, i);
+        i += di/2;
+        strncpy(output, delim+(di-di/2), di/2);
+        strncpy(output+i-1, delim, di-di/2);
+        i += di-di/2;
+        printf("%d %d", isLast, LENGTH(blocks));
+        if (isLast) output[i-1] = ' ';
+    } else {
+        i--;
+    }
+    output[i] = '\0';
+    printf("%s\n", output);
+    pclose(cmdf);
 }
 
 void getcmds(int time)
 {
-	const Block* current;
-	for(int i = 0; i < LENGTH(blocks); i++)
-	{
-		current = blocks + i;
-		if ((current->interval != 0 && time % current->interval == 0) || time == -1)
-			getcmd(current, statusbar[i]);
-	}
+    const Block* current;
+    for(int i = 0; i < LENGTH(blocks); i++)
+    {
+        current = blocks + i;
+        if ((current->interval != 0 && time % current->interval == 0) || time == -1)
+            getcmd(current, statusbar[i], i+1 == LENGTH(blocks));
+    }
 }
 
 #ifndef __OpenBSD__
 void getsigcmds(int signal)
 {
-	const Block *current;
-	for (int i = 0; i < LENGTH(blocks); i++)
-	{
-		current = blocks + i;
-		if (current->signal == signal)
-			getcmd(current, statusbar[i]);
-	}
+    const Block *current;
+    for (int i = 0; i < LENGTH(blocks); i++)
+    {
+        current = blocks + i;
+        if (current->signal == signal)
+            getcmd(current, statusbar[i], i+1 == LENGTH(blocks));
+    }
 }
 
 void setupsignals()
 {
-	for(int i = 0; i < LENGTH(blocks); i++)
-	{
-		if (blocks[i].signal > 0)
-			signal(SIGRTMIN+blocks[i].signal, sighandler);
-	}
+    for(int i = 0; i < LENGTH(blocks); i++)
+    {
+        if (blocks[i].signal > 0)
+            signal(SIGRTMIN+blocks[i].signal, sighandler);
+    }
 
 }
 #endif
 
 int getstatus(char *str, char *last)
 {
-	strcpy(last, str);
-	str[0] = '\0';
-	for(int i = 0; i < LENGTH(blocks); i++)
-		strcat(str, statusbar[i]);
-	str[strlen(str)-1] = '\0';
-	return strcmp(str, last);//0 if they are the same
+    strcpy(last, str);
+    str[0] = '\0';
+    for(int i = 0; i < LENGTH(blocks); i++)
+        strcat(str, statusbar[i]);
+    str[strlen(str)-1] = '\0';
+    return strcmp(str, last);//0 if they are the same
 }
 
 void setroot()
 {
-	if (!getstatus(statusstr[0], statusstr[1]))//Only set root if text has changed.
-		return;
-	Display *d = XOpenDisplay(NULL);
-	if (d) {
-		dpy = d;
-	}
-	screen = DefaultScreen(dpy);
-	root = RootWindow(dpy, screen);
-	XStoreName(dpy, root, statusstr[0]);
-	XCloseDisplay(dpy);
+    if (!getstatus(statusstr[0], statusstr[1]))//Only set root if text has changed.
+        return;
+    Display *d = XOpenDisplay(NULL);
+    if (d) {
+        dpy = d;
+    }
+    screen = DefaultScreen(dpy);
+    root = RootWindow(dpy, screen);
+    XStoreName(dpy, root, statusstr[0]);
+    XCloseDisplay(dpy);
 }
 
 void pstdout()
 {
-	if (!getstatus(statusstr[0], statusstr[1]))//Only write out if text has changed.
-		return;
-	printf("%s\n",statusstr[0]);
-	fflush(stdout);
+    if (!getstatus(statusstr[0], statusstr[1]))//Only write out if text has changed.
+        return;
+    printf("%s\n",statusstr[0]);
+    fflush(stdout);
 }
 
 
 void statusloop()
 {
 #ifndef __OpenBSD__
-	setupsignals();
+    setupsignals();
 #endif
-	int i = 0;
-	getcmds(-1);
-	while(statusContinue)
-	{
-		getcmds(i);
-		writestatus();
-		sleep(1.0);
-		i++;
-	}
+    int i = 0;
+    getcmds(-1);
+    while(statusContinue)
+    {
+        getcmds(i);
+        writestatus();
+        sleep(1.0);
+        i++;
+    }
 }
 
 #ifndef __OpenBSD__
 void sighandler(int signum)
 {
-	getsigcmds(signum-SIGRTMIN);
-	writestatus();
+    getsigcmds(signum-SIGRTMIN);
+    writestatus();
 }
 #endif
 
 void termhandler(int signum)
 {
-	statusContinue = 0;
-	exit(0);
+    statusContinue = 0;
+    exit(0);
 }
 
 int main(int argc, char** argv)
 {
-	for(int i = 0; i < argc; i++)
-	{
-		if (!strcmp("-d",argv[i]))
-			*delim = argv[++i][0];
-		else if(!strcmp("-p",argv[i]))
-			writestatus = pstdout;
-	}
-	signal(SIGTERM, termhandler);
-	signal(SIGINT, termhandler);
-	statusloop();
+    for(int i = 0; i < argc; i++)
+    {
+        if (!strcmp("-d",argv[i]))
+            delim = argv[++i];
+        else if(!strcmp("-p",argv[i]))
+            writestatus = pstdout;
+    }
+    signal(SIGTERM, termhandler);
+    signal(SIGINT, termhandler);
+    statusloop();
 }
