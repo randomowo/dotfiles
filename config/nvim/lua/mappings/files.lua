ff579a2d900aca162713415c2b6bf5022b43adf4
randomowo <dev@randomowo.dev>
2023-11-19 16:24:53 +0300

    improved files run

--- config/nvim/lua/mappings/files.lua
+++ config/nvim/lua/mappings/files.lua
@@ -14,7 +14,7 @@ function run_file()
         if vim.bo.filetype == obj.filetype then
             vim.cmd('write!')
             local filepath = vim.fn.expand('%')
-            vim.cmd.wincmd('v')
+            vim.cmd.vnew()
             vim.cmd.terminal()
             vim.api.nvim_chan_send(vim.bo.channel, table.concat(obj.cmd, ' ') .. ' ' .. filepath ..'\r')
         end
@@ -22,4 +22,30 @@ function run_file()
 end
 
 alias.map('<F9>', run_file, { buffer = true })
+
+function close_term()
+    vim.cmd.startinsert()
+    local stop = vim.api.nvim_replace_termcodes('<C-c>', true, false, true)
+    vim.api.nvim_feedkeys(stop, 't', false)
+    local exit = vim.api.nvim_replace_termcodes('<C-d>', true, false, true)
+    vim.api.nvim_feedkeys(exit, 't', true)
+end
+vim.api.nvim_create_autocmd(
+    { 'TermOpen' },
+    {
+        pattern = {'*'},
+        callback = function()
+            alias.map('<F9>', close_term, { buffer = true })
+        end,
+    }
+)
+vim.api.nvim_create_autocmd(
+    { 'TermClose' },
+    {
+        pattern = {'*'},
+        callback = function()
+            vim.cmd(':bd')
+        end,
+    }
+)
 
