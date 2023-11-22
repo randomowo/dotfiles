d9ff72452cb47aa0c8258eb82d1a721b569297d2
randomowo <dev@randomowo.dev>
2023-11-22 20:08:39 +0300

    treesitter config auto_install -> false

--- config/nvim/lua/settings/treesitter.lua
+++ config/nvim/lua/settings/treesitter.lua
@@ -12,7 +12,7 @@ local export = {
             'gitignore',
         },
         sync_install = false,
-        auto_install = true,
+        auto_install = false,
         highlight = {
             enable = true,
             additional_vim_regex_highlighting = false,
