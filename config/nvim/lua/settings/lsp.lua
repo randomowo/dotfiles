9360394c49d0f6f6caa2d442df131e831083d884
randomowo <dev@randomowo.dev>
2024-08-16 14:09:41 +0300

    chore: nvim lsp pyright -> pylsp

--- config/nvim/lua/settings/lsp.lua
+++ config/nvim/lua/settings/lsp.lua
@@ -1,6 +1,6 @@
 local export = {
     configs = {
-        { name = 'pyright', settings = {}, preload_cmd = { 'npm', 'i', '-g', 'pyright' } },
+        { name = 'pylsp', settings = {} },--, preload_cmd = { 'npm', 'i', '-g', 'pyright' } },
         { name = 'gopls', settings = {}, preload_cmd = { 'go', 'install', 'golang.org/x/tools/gopls@latest' } },
         { name = 'tsserver', settings = {}, preload_cmd = { 'npm', 'i', '-g', 'typescript-language-server' } },
         { name = 'dockerls', settings = {}, preload_cmd = { 'npm', 'i', '-g', 'dockerfile-language-server-nodejs' } },
