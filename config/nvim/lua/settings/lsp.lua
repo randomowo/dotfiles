ce6bd4364a8f9b43e85ddc2809c2c3bacddf6f32
randomowo <dev@randomowo.dev>
2024-09-11 16:35:59 +0300

    fix: fixed deprecated lsp

--- config/nvim/lua/settings/lsp.lua
+++ config/nvim/lua/settings/lsp.lua
@@ -2,7 +2,7 @@ local export = {
     configs = {
         { name = 'pylsp', settings = {} },--, preload_cmd = { 'npm', 'i', '-g', 'pyright' } },
         { name = 'gopls', settings = {}, preload_cmd = { 'go', 'install', 'golang.org/x/tools/gopls@latest' } },
-        { name = 'tsserver', settings = {}, preload_cmd = { 'npm', 'i', '-g', 'typescript-language-server' } },
+        { name = 'ts_ls', settings = {} }, -- preload_cmd = { 'npm', 'i', '-g', 'typescript-language-server' } },
         { name = 'dockerls', settings = {}, preload_cmd = { 'npm', 'i', '-g', 'dockerfile-language-server-nodejs' } },
         { name = 'rust_analyzer', settings = {}, preload_cmd = { 'rustup', 'component', 'add', 'rust-analyzer' } },
     }
